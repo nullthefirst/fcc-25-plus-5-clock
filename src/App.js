5c3dccf9c08074967b5604dc034a0b2fb5a3e388
Ninte <ninte.dev@gmail.com>
2021-07-31 10:04:33 +0100

    Integrated freecodecamp test package component.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
+import ReactFCCtest from 'react-fcctest';
+
 import './App.css';
 import Pomodoro from './pomodoro';
 
 function App() {
   return (
     <div id="app">
+      <ReactFCCtest />
       <Pomodoro />
     </div>
   );
